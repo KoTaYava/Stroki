129f46bc0fa0fe257162cf68094612b42a655cb3
KoTaYava <146347467+KoTaYava@users.noreply.github.com>
2023-10-30 18:05:36 +0300

    stroki

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String firsName = " Ivan";
+        String middName = " Ivanovich";
+        String fullName = " Ivanov";
+        String fiName = "Семён ";
+        String miName = "Семёнович";
+        String fuName = "Иванов";
+        fiName.replace("ё", "е");
+        miName.replace("ё", "е");
+        fuName.replace("ё", "е");
+        System.out.println(fullName + firsName + middName );
+        System.out.println("Данные Ф.И.О сотрудника для заполнения отчета " + fullName.toUpperCase() + firsName.toUpperCase() + middName.toUpperCase());
+        System.out.println("Данные Ф.И.О сотрудника для заполнения отчета " + fuName + fiName + miName);
+    }
+}
